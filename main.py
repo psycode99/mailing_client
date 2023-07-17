f6ceee731a2edabae9eb46c1ae0d165ef183507b
psycode99 <georgefavour694@gmail.com>
2023-07-17 07:28:35 -1200

    initial commit

---
+++ main.py
@@ -0,0 +1,36 @@
+import smtplib, ssl
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email.mime.multipart import MIMEMultipart
+from email import encoders
+
+credentials = __import__("credentials")
+email_address = credentials.email
+password = credentials.password
+
+msg = MIMEMultipart()
+msg['From'] = "Mad Max"
+msg["To"] = "pydevelops@gmail.com"
+msg["Subject"] = "Hello nerd, its Mad Max"
+
+with open("message.txt", 'r') as message:
+    subject = message.read()
+    msg.attach(MIMEText(subject, "plain"))
+
+attached_file = "mad_max.png"
+with open(attached_file, "rb") as attachment:
+    payload = MIMEBase("application", "octet-stream")
+    payload.set_payload(attachment.read())
+
+    encoders.encode_base64(payload)
+    payload.add_header("Content-Disposition", f"attachment; filename={attached_file}")
+    msg.attach(payload)
+
+
+text = msg.as_string()
+context = ssl.create_default_context()
+with smtplib.SMTP("smtp.gmail.com", 587) as server:
+    server.starttls()
+    #server.ehlo()
+    server.login(email_address, password)
+    server.sendmail(email_address, "pydevelops@gmail.com", text)
